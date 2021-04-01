a36db9c2e0c7d4f8609fb8dd8d3825ba3671ad7a
Abel Maldonado Vasquez <abelmaldonadov@outlook.com>
2021-04-01 17:06:12 -0500

    primer commit

---
+++ scripts/core.js
@@ -0,0 +1,28 @@
+document.querySelector("#start").addEventListener("click",start)
+
+const ficha = "";
+
+function start() {
+    // 
+}
+
+function jugarO() {
+    // 
+}
+
+function jugarX() {
+    // 
+}
+
+function limpiar() {
+    // 
+}
+
+function showMessage(tipo) {
+    // MENSAJE BUENO O MALO BOOLEAN
+    if (tipo) {
+        alert("Has ganado")
+    } else {
+        alert("Has perdido")
+    }
+}
